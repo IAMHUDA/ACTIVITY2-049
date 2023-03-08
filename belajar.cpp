7f00c756fb828eab5a57567b412f78d3b643caa3
IAMHUDA <hudamiftahul362@gmail.com>
2023-03-08 16:03:16 +0700

    first commit

---
+++ belajar.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Begin 
+    //numeric luas p,L
+    double luas,p,l;
+    //display 'masukan panjangnya'
+    cout << "masukan panjangnhya = ";
+    //accept
+    cin >> p;
+    //display 'masukan lebarnya'
+    cout << "masukan lebarnya = ";
+    //accept
+    cin >> l;
+    //compute luas =p*l
+    luas = p*l;
+    // masukan luasnya
+    cout << "luasnya = " << luas;
+    //end
+    return 0;
+                                                                                                            
+}
